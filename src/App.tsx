4c14599dabf03be34ae893e05537eaa64998326d
Douglas Freitas <douglasamf14@gmail.com>
2025-05-09 17:19:23 -0300

    Feature: added switch theme button and created a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import { LoginPage } from "./pages"
 import { useTheme } from "./hooks"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"
 
 function App() {
 
   const toggleTheme = useTheme()[1]
 
   return (
-    <main>
-      <LoginPage/>
-      <button
-      onClick={() => toggleTheme()}>aaa</button>
-    </main>
+    <>
+      <header>
+        <div className="toggle-theme-container">
+          <button className="toggle-theme-button"
+            onClick={() => toggleTheme()}
+          >
+            <FontAwesomeIcon 
+              icon={
+                localStorage.getItem("theme") === "dark" 
+                ? faSun : faMoon
+              } 
+            />
+          </button>
+        </div>
+      </header>
+      <main>
+        <LoginPage/>
+      </main>
+    </>
   )
 }
 
